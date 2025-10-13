c499aea95b9f07eb3ac98a7a3456d14082bd03d3
nikkicnikki <aspicris@gmail.com>
2025-10-13 12:50:53 +0800

    remove dashboard.tsx
    because im not using it!

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,9 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    // Route::get('dashboard', function () {
+    //     return Inertia::render('dashboard');
+    // })->name('dashboard');
 });
 
 require __DIR__ . '/settings.php';
