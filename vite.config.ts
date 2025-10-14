ebd8be79e48e7703ab4f8a3ffc4ed40adbe0ea03
nikkicnikki <aspicris@gmail.com>
2025-10-14 09:29:58 +0800

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -27,12 +27,13 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: 'https://tcu-website-sample.onrender.com/', // ✅ Force HTTPS asset URLs
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
-            buildDirectory: 'build', // ✅ important for v2.x
+            buildDirectory: 'build',
         }),
         react(),
         tailwindcss(),
